82aeb0d8d0e7a9be875a49fa9d682e76d10ccf76
md-ratul-gazi <mdratulgazi001@gmail.com>
2023-09-13 09:26:49 +0600

    Class 3 done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,22 +1,27 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
   // Declare useState
-  const [Countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
   // Load Data using useEffect
   useEffect(() => {
     const handleData = async () => {
       const res = await fetch("https://restcountries.com/v3.1/all");
       const data = await res.json();
-      console.log(data);
+      setCountries(data);
     };
     handleData(); // call the handleData function.
   }, []);
   return (
     <div>
-      <h3>Countries</h3>
+      <h3>Countries: {countries.length}</h3>
+      {countries.map((country) => (
+        // Set a uniq key to help find the data in react virtual dom
+        <Country key={country.cca3} country={country}></Country>
+      ))}
     </div>
   );
 };
